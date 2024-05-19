24f64e34eff64c243638ad2db5080ab540e8c1d5
NastyNastya2024 <166286695+NastyNastya2024@users.noreply.github.com>
2024-05-19 11:14:45 +0300

    add new class

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-a,b = 8,9
-c, d = 4,5
+class Warrior():
+    def __init__(self, name, power, endurance, hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
 
-if a > b:
-    print("a больще чем b")
-elif a < b:
-    print("b больще чем a")
-else:
-    print("a равно b")
+    def sleep (self):
+        print(f"{self.name} is sleeping...")
+        self.endurance += 2
+
+    def eat(self):
+        print(f"{self.name} is eating...")
+        self.power += 2
+
+    def hit(self):
+        print(f"{self.name} is hitting...")
+        self.endurance -= 6
+
+    def walk(self):
+        print(f"{self.name} is walking...")
+
+    def info (self):
+        print(f"Warrior's name {self.name} ")
+        print(f"Warrior's power {self.power} ")
+        print(f"Warrior's endurance {self.endurance} ")
+        print(f"Warrior's hair color {self.hair_color}")
+
 
